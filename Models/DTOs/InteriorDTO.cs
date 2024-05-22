fecd1c86e6cf4f83954510b76bbe87f62735bed6
Eric Heidel <ericleeheidel@yahoo.com>
2024-05-22 18:08:03 -0500

    added classes and DTOs

---
+++ Models/DTOs/InteriorDTO.cs
@@ -0,0 +1,8 @@
+namespace CarBuilder.Models;
+
+public class InteriorDTO
+{
+    public int Id { get; set; }
+    public decimal Price { get; set; }
+    public string? Material { get; set; }
+}
---
+++ Models/DTOs/OrderDTO.cs
@@ -0,0 +1,9 @@
+public class OrderDTO
+{
+    public int Id { get; set; }
+    public DateTime TimeStamp { get; set; }
+    public int WheelId { get; set; }
+    public int TechnologyId { get; set; }
+    public int PaintId { get; set; }
+    public int InteriorId { get; set; }
+}
